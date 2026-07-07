7153a4f6cb6f56bfc9a97e54414017fb330c4710
Monday Lin <mondaylin999@gmail.comm>
2026-07-08 01:27:51 +0630

    Update exe1.py to version 2 with Dog subclass

--- OOP Concept/Inheritance/exe1.py
+++ OOP Concept/Inheritance/exe1.py
@@ -3,11 +3,15 @@ class Animal:
         self.name = name
 
     def sound(self):
-        return f'{self.name} makes a sound.'
+        return f'{self.name} makes a sound'
 
 class Dog(Animal):
     bark = 'woof! woof!! woof!!!'
 
+    # Call Animal.sound(), then append bark
+    def sound(self):
+        base = super().sound()
+        return f'{base}, then {self.name} barks {self.bark}'
+
 jack = Dog('Jack')
-print(jack.sound())  # Jack makes a sound
-print(jack.bark)  # woof! woof!! woof!!!
+print(jack.sound())  # Jack makes a sound, then Jack barks woof! woof!! woof!!!
